a1728c6fa1231b325cb660205be5a2b4d502ba23
Benj Fassbind <randombenj@gmail.com>
2020-10-09 20:24:37 +0200

    Refactor all the things

--- backend/taxy/document_analyzer.py
+++ backend/taxy/document_analyzer.py
@@ -18,31 +18,81 @@ DOCUMENT_CLASSIFYER = {
     DocumentType.BILL: lambda content: "rechnung" in content.lower()
 } 
 
-DOCUMENT_DATA_EXTRACTOR = {
-    DocumentType.WAGE_CARD: lambda content: {
-        "amount": int(re.compile(r"nettolohn(.|\n)*?((\d|')+)", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][1].replace("'", "")),
-    },
-    DocumentType.INTEREST_STATEMENT: lambda content: {
-        # match the saldo
-        "amount": float(re.compile(r"saldo(.|\n)*?((\d|'|)+\.\d\d)$", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][1].replace("'", "")),
-        # match the zins
-        "interest": float(re.compile(r"habenzins(.|\n)*?((\d|'|)+\.\d\d)$", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][1].replace("'", "")),
-        "year": re.compile(r"31\.12\.(\d{2,4})", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0]
-    },
-    #DocumentType.INSURACNCE_STATMENT = 3
-    DocumentType.BILL: lambda content: {
-        "amount": float(re.compile(r"(end|gesamt|schluss)betrag(.)*?((\d|'|)+\.\d\d)$", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][2].replace("'", ""))
+
+def __get_match(content, indexes, name, data_extractor):
+    entry = {"x": 0, "y": 0, "height": 0, "width": 0}
+
+    #try:
+    match, original_match = data_extractor(content)
+
+    match_index = indexes["text"].index(original_match)
+    return {
+        "height": indexes["height"][match_index],
+        "width": indexes["width"][match_index],
+        "top": indexes["top"][match_index],
+        "left": indexes["left"][match_index],
+        name: match
     }
+            
+    """except:
+        return {
+            "height": 0,
+            "width": 0,
+            "top": 0,
+            "left": 0,
+            name: 0
+        }  # well we fucked up (╯°□°)╯︵ ┻━┻"""
+
+def __wage_data_extractor(content):
+    original_match = re.compile(r"nettolohn(.|\n)*?((\d|')+)", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][1]
+    match = int(original_match.replace("'", ""))
+    return match, original_match
+
+def __interest_amount_data_extractor(content):
+    original_match = re.compile(r"saldo(.|\n)*?((\d|'|)+\.\d\d)$", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][1]
+    match = float(original_match.replace("'", ""))
+    return match, original_match
+
+def __interest_data_extractor(content):
+    original_match = re.compile(r"habenzins(.|\n)*?((\d|'|)+\.\d\d)$", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][1]
+    match = float(original_match.replace("'", ""))
+    return match, original_match
+
+def __year_data_extractor(content):
+    m = re.compile(r"31\.12\.(\d{2,4})", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0]
+    return m, f"31.12.{m}"
+
+def __bill_data_extractor(content):
+    original_match = re.compile(r"(end|gesamt|schluss)betrag(.)*?((\d|'|)+\.\d\d)$", flags=re.IGNORECASE|re.MULTILINE).findall(content)[0][2]
+    match = float(original_match.replace("'", ""))
+    return match, original_match
+
+
+DOCUMENT_DATA_EXTRACTOR = {
+    DocumentType.WAGE_CARD: lambda content, indexes: [
+        __get_match(content, indexes, "amount", __wage_data_extractor)
+    ],
+    DocumentType.INTEREST_STATEMENT: lambda content, indexes: [
+        __get_match(content, indexes, "amount", __interest_amount_data_extractor),
+        __get_match(content, indexes, "interest", __interest_data_extractor),
+        __get_match(content, indexes, "year", __year_data_extractor)
+    ],
+    #DocumentType.INSURACNCE_STATMENT = 3
+    DocumentType.BILL: lambda content, indexes: [
+        __get_match(content, indexes, "amount", __bill_data_extractor)
+    ]
 }
 
 
-def classify_document(document_content):
+def classify_document(document_content, indexes):
     """
     Classifies what a the type of an upploaded document is
     """
     for classifyer in DOCUMENT_CLASSIFYER:
         if DOCUMENT_CLASSIFYER[classifyer](document_content):
-            return (classifyer.value, DOCUMENT_DATA_EXTRACTOR[classifyer](document_content))
+            result = (classifyer.value, DOCUMENT_DATA_EXTRACTOR[classifyer](document_content, indexes))
+            print(result)
+            return result
 
 
 def scan_document(document_path):
@@ -53,6 +103,8 @@ def scan_document(document_path):
         images = [Image.open(str(document_path))]
 
     # Simple image to string
-    result = "".join(pytesseract.image_to_string(image, lang='deu') for image in images)
-    print(result)
-    return classify_document(result)
+    text_result = "".join(pytesseract.image_to_string(image, lang='deu') for image in images)
+    indexes = pytesseract.image_to_data(images[0], lang="deu", output_type=pytesseract.Output.DICT)
+    #FIXME: ? text_result = " ".join(indexes["text"])
+    print(" ".join(indexes["text"]))
+    return classify_document(text_result, indexes)
