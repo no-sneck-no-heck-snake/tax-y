8ecd5b542a608c2c0d022215a90bc24a2f7656b8
Livio Brunner <livio.brunner.lb1@gmail.com>
2020-10-09 15:53:34 +0200

    add fancy cards

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { Jumbo } from "../components/Jumbo";
+import { Grid } from "@material-ui/core";
 
 export function Home() {
   return (
     <>
-      <Jumbo></Jumbo>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
+          <Jumbo title="Vermögen" image="/images/vermoegen.jpg">
+            Verwalte dein Vermögen und lade deine Dokumente hoch
+          </Jumbo>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Jumbo title="Abzüge" image="/images/abzuege.jpg">
+            Verwalte deine Abzüge und lade deine Dokumente hoch
+          </Jumbo>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Jumbo title="Einkünfte" image="/images/einkuenfte.png">
+            Verwalte dein Einkünfte und lade deine Dokumente hoch
+          </Jumbo>
+        </Grid>
+      </Grid>
     </>
   );
 }
