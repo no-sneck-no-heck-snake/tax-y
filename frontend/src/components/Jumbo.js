1b3e31200a5b094993f1b90519e777e063200ddc
Stephan Stofer <stephan.stofer@hslu.ch>
2020-10-09 14:55:41 +0200

    remove import

--- frontend/src/components/Jumbo.js
+++ frontend/src/components/Jumbo.js
@@ -7,7 +7,6 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import {ListTest} from "./ListTest";
 import {List} from "@material-ui/core";
 
 const useStyles = makeStyles({
