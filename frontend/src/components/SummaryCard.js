31aaac90a3e09d48a33a727e57ab6aef05195ffa
Livio Brunner <livio.brunner.lb1@gmail.com>
2020-10-09 16:35:52 +0200

    card refactor

---
+++ frontend/src/components/SummaryCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  Card as _Card,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import styled from "styled-components";
+
+const Card = styled(_Card)`
+  box-shadow: 0 2.8px 3.3px rgba(0, 0, 0, 0.028),
+    0 6.7px 6.9px rgba(0, 0, 0, 0.044), 0 12.5px 11.3px rgba(0, 0, 0, 0.056),
+    0 22.3px 17.5px rgba(0, 0, 0, 0.067), 0 41.8px 29.4px rgba(0, 0, 0, 0.074),
+    0 100px 71px rgba(0, 0, 0, 0.07) !important;
+`;
+
+export function SummaryCard({ title }) {
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {title}
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { Jumbo } from "../components/Jumbo";
-import { Grid } from "@material-ui/core";
+import { SummaryCard } from "../components/SummaryCard";
+
+
+{
+  /* <Grid container spacing={3}>
+    <Grid item xs={12} md={4}>
+      <Jumbo title="Vermögen" image="/images/vermoegen.jpg">
+        Verwalte dein Vermögen und lade deine Dokumente hoch
+      </Jumbo>
+    </Grid>
+    <Grid item xs={12} md={4}>
+      <Jumbo title="Abzüge" image="/images/abzuege.jpg">
+        Verwalte deine Abzüge und lade deine Dokumente hoch
+      </Jumbo>
+    </Grid>
+    <Grid item xs={12} md={4}>
+      <Jumbo title="Einkünfte" image="/images/einkuenfte.png">
+        Verwalte dein Einkünfte und lade deine Dokumente hoch
+      </Jumbo>
+    </Grid>
+  </Grid> */
+}
 
 export function Home() {
-  return (
-    <>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
-          <Jumbo title="Vermögen" image="/images/vermoegen.jpg">
-            Verwalte dein Vermögen und lade deine Dokumente hoch
-          </Jumbo>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Jumbo title="Abzüge" image="/images/abzuege.jpg">
-            Verwalte deine Abzüge und lade deine Dokumente hoch
-          </Jumbo>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Jumbo title="Einkünfte" image="/images/einkuenfte.png">
-            Verwalte dein Einkünfte und lade deine Dokumente hoch
-          </Jumbo>
-        </Grid>
-      </Grid>
-    </>
-  );
+ return <div>
+   <SummaryCard title="Vermögen"></SummaryCard>
+ </div>
 }
