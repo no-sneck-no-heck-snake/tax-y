032eab1cf4b4b9d5d35aca7e502cfd682e171ae0
Lukas Weber <devlukas@hotmail.com>
2020-10-09 14:54:58 +0200

    Add media viewer

---
+++ frontend/src/components/MediaViewer.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const MediaContainer = styled.div`
+  position: relative;
+`;
+
+const HighlightContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const Highlight = styled.div`
+  position: absolute;
+  border: 1px solid rgb(255,0,0);
+  background-color: rgba(3, 152, 252, 0.5);
+`; 
+
+export function MediaViewer(props) {
+  const { src, highlights } = props;
+
+  return (
+    <MediaContainer>
+        <img src={{src}}></img>
+        <HighlightContainer>
+          { highlights.map(h => <Highlight style={{ 
+            top: `${h.y}px`, 
+            left: `${h.x}px`,
+            height: `${h.height}px`,
+            width: `${h.width}px`,
+          }}></Highlight>) }
+        </HighlightContainer>
+    </MediaContainer>
+  );
+}
