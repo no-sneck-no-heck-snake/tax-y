b9d3c461fd61f94ff90c3e71dd34a8774779c0e5
Livio Brunner <livio.brunner.lb1@gmail.com>
2020-10-10 08:36:59 +0200

    responsive chart whuat

--- frontend/src/components/DeductionChart.js
+++ frontend/src/components/DeductionChart.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
-import { getCategoryColor } from "../category";
 import { formatFranks } from "../util";
 
 export function DeductionChart({ data }) {
+  const [mounted, setMounted] = useState(false);
   const categories = data.categories.map((category) => ({
     name: category.displayName,
     value: category.maxDeduction || category.currentDeduction,
+    color: category.color,
   }));
 
   const categoriesOuterRadiant = [];
@@ -29,42 +30,41 @@ export function DeductionChart({ data }) {
     });
   }
 
-  return (
-    <ResponsiveContainer width="80%" height={250}>
-      <PieChart
-        width={300}
-        height={300}
+  useEffect(() => setTimeout(() => setMounted(true), 500), [setMounted]);
+
+  return mounted ? (
+    <PieChart width={400} height={400} margin={{ left: 50, top: 50 }}>
+      <Pie
+        data={[...categories]}
+        dataKey="value"
+        cx={150}
+        cy={150}
+        outerRadius={45}
+        fill="#8884d8"
       >
-        <Pie
-          data={categories}
-          dataKey="value"
-          cx={100}
-          cy={100}
-          outerRadius={30}
-          fill="#8884d8"
-        >
-          {categories.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={getCategoryColor(entry.name)} />
-          ))}
-        </Pie>
-        <Pie
-          data={categoriesOuterRadiant}
-          dataKey="value"
-          cx={100}
-          cy={100}
-          innerRadius={35}
-          outerRadius={45}
-          fill="#82ca9d"
-          label
-          cornerRadius={10}
-          paddingAngle={5}
-          label={({ value }) => formatFranks(value)}
-        >
-          {categoriesOuterRadiant.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={entry.color} />
-          ))}
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
+        {categories.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={entry.color} />
+        ))}
+      </Pie>
+      <Pie
+        data={[...categoriesOuterRadiant]}
+        dataKey="value"
+        cx={150}
+        cy={150}
+        innerRadius={50}
+        outerRadius={67}
+        fill="#82ca9d"
+        label
+        cornerRadius={10}
+        paddingAngle={5}
+        label={({ value }) => formatFranks(value)}
+      >
+        {categoriesOuterRadiant.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={entry.color} />
+        ))}
+      </Pie>
+    </PieChart>
+  ) : (
+    <></>
   );
 }
