83b66d26fe27656c089ae966b2697319a541e595
Benj Fassbind <randombenj@gmail.com>
2020-10-10 03:08:01 +0200

    don't do the cache

--- backend/taxy/app.py
+++ backend/taxy/app.py
@@ -45,6 +45,18 @@ def make_app():
     mongo = PyMongo(app)
     app.mongo = mongo
 
+    @app.after_request
+    def add_header(r):
+        """
+        Add headers to both force latest IE rendering engine or Chrome Frame,
+        and also to cache the rendered page for 10 minutes.
+        """
+        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Pragma"] = "no-cache"
+        r.headers["Expires"] = "0"
+        r.headers['Cache-Control'] = 'public, max-age=0'
+        return r
+
     @app.route("/document", methods=["POST"])
     def root():
         user_session = request.cookies.get('_taxy_session', 0)
